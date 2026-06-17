e75eded56f2d84d4b88959d90620c991252854b3
rayeesafathima-intern <intern-rayeesa@rootpro.in>
2026-06-17 21:57:32 +0530

    debug db env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,22 @@
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "rayfath24@sql1177",
-  database: "inventory_db",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 console.log("MySQL Pool Connected");
-
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_PORT =", process.env.DB_PORT);
+console.log("DB_NAME =", process.env.DB_NAME);
+console.log("DB_USER =", process.env.DB_USER);
 module.exports = db;
